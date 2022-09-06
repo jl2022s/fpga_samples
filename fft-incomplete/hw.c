452cad9c1c282cf3263125bf5d63e6851f352f84
jl2022s <108269049+jl2022s@users.noreply.github.com>
2022-09-06 16:19:58 +0100

    Add files via upload

---
+++ fft-incomplete/hw.c
@@ -0,0 +1,65 @@
+#include "hw.h"
+
+void ex_sort(int in[N], int out[N])
+{
+	ap_uint<M> work_array[N];
+#pragma HLS ARRAY_PARTITION variable=work_array cyclic factor=2 dim=0
+	ap_uint<M> work_array2[N];
+#pragma HLS ARRAY_PARTITION variable=work_array2 cyclic factor=2 dim=0
+
+	//1. Fill in the work array
+init_loop:
+	for (unsigned i = 0; i < N; i++) {
+#pragma HLS PIPELINE
+		work_array[i] = in[i];
+	}
+
+	//2. Sort the data
+proc_loop:
+	for (int n = 0; n < N / 2; n++) {
+		// processing even pairs of registers
+		// [0, 1], [2, 3], [4 ...
+	sort_even:
+		for (unsigned j = 0; j < (N / 2); j++){
+#pragma HLS UNROLL
+			if (work_array[2 * j] < work_array[2 * j + 1]) {
+				work_array2[2 * j + 1] = work_array[2 * j];
+				work_array2[2 * j]     = work_array[2 * j + 1];
+			}else{
+				work_array2[2 * j]     = work_array[2 * j];
+				work_array2[2 * j + 1] = work_array[2 * j + 1];
+			}
+
+			if(N % 2 == 1){
+				work_array[N - 1] = work_array2[N - 1];
+			}
+		}
+
+		// processing odd pairs of registers
+		// 0, [1, 2], [3, 4] ...
+	sort_odd:
+		for (unsigned j = 0; j < (N / 2 - 1); j++){
+#pragma HLS UNROLL
+			work_array[0] = work_array2[0];
+
+			if (work_array2[2 * j + 1] < work_array2[2 * j + 2]) {
+				work_array[2 * j + 1] = work_array2[2 * j + 2];
+				work_array[2 * j + 2] = work_array2[2 * j + 1];
+			}else{
+				work_array[2 * j + 1] = work_array2[2 * j + 1];
+				work_array[2 * j + 2] = work_array2[2 * j + 2];
+			}
+
+			if(N % 2 == 0){
+				work_array[N - 1] = work_array2[N - 1];
+			}
+		}
+	}
+
+	//3. Write the result
+write_res_loop:
+	for (unsigned i = 0; i < N; i++) {
+#pragma HLS PIPELINE
+		out[i] = work_array[i];
+	}
+}
---
+++ fft-incomplete/hw.h
@@ -0,0 +1,10 @@
+#ifndef _HW_H_
+#define _HW_H_
+
+#include 	"ap_int.h"
+#define		M	32 //number of bits in each data item
+#define		N	16 //number of data items
+
+void ex_sort(int in[N], int out[N]);
+
+#endif // _HW_H_
